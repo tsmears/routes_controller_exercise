7d33311baae371273ba76e5c09fba0014d08907b
Taylor Smears <tsmears17@gmail.com>
2013-11-24 22:35:56 +0100

    Added ability to create new products

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ ControllerExercise::Application.routes.draw do
 
 
   get '/products' => 'products#index'
+  get '/products/new' => 'products#new'
+
+  post '/products' => 'products#create'
 
   resources :users
 
